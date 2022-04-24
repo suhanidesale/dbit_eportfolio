16a4b8ab90393c694e0f8f3bfe2ea9fa92bc21d9
Avin Dsouza <avinalricdsouza@gmail.com>
2022-04-24 17:35:53 +0530

    core update

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList =[
+    {
+    'id':'1',
+    'title':"Ecommerce Website",
+    'descrpition':'Fully functional ecommerce website'
+    },
+    {
+    'id':'2',
+    'title':"Portfolio Website",
+    'description': 'This was aproject where I built out my portfolio'    
+    },
+            
+] 
+
 def projects(request):
-    return render(request,'projects/projects.html')
+    page = 'projects'
+    number = 11
+    context ={'page': page , 'number': number , 'projects': projectsList}
+    return render(request,'projects/projects.html',context)
 
 def project(request, pk):
-    return render(request,'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+           projectObj = i
+    return render(request,'projects/single-project.html',{'project':projectObj})
 
