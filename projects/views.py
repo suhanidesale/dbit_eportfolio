a82d805f86a6673fe0de41ea62554ba6ce6817bd
Avin Dsouza <avinalricdsouza@gmail.com>
2022-04-26 02:26:54 +0530

    updated model forms

--- projects/views.py
+++ projects/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-projectsList =[
-    {
-    'id':'1',
-    'title':"Ecommerce Website",
-    'descrpition':'Fully functional ecommerce website'
-    },
-    {
-    'id':'2',
-    'title':"Portfolio Website",
-    'description': 'This was aproject where I built out my portfolio'    
-    },
-            
-] 
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
@@ -22,7 +10,11 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
+   
     print('projectObj:',projectObj)
-    return render(request,'projects/single-project.html',{'project':projectObj},'tags': tags)
+    return render(request,'projects/single-project.html',{'project':projectObj})
 
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, "projects/project_form.html",context )
